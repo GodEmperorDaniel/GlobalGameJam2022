03bcf48086943a375d9a5befb8e0a404b9c152b8
karl <Karl.baecklund@gmail.com>
2022-01-29 15:22:42 +0100

    Auto stash before merge of "Programmering" and "origin/Programmering"

--- Assets/UIManager.cs
+++ Assets/UIManager.cs
@@ -7,21 +7,21 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public static UIManager UI;
-    public GameObject defaulButton;
-
+    public GameObject defaulButton;
+
     private void Awake()
     {
         UI = this;
     }
-    public bool CheckIfBothSelectedCharacter()
-    {
-        foreach (CharacterInformation p in CharacterInformation.players)
-        {
-            if (p.GetComponent<UIInputs>().choosenCharacter == CharacterENUM.NONE)
-            {
-                return false;
-            }
-        }
+    public bool CheckIfBothSelectedCharacter()
+    {
+        foreach (CharacterInformation p in CharacterInformation.players)
+        {
+            if (p.GetComponent<UIInputs>().choosenCharacter == CharacterENUM.NONE)
+            {
+                return false;
+            }
+        }
         return true;
     }
 }
